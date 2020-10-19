9cc2f46e0ff96d7ec492fb956b7a24d197c79e7b
uloydev <uloydev@gmail.com>
2020-10-19 14:57:14 +0700

    [FIX] fix time validation

--- PostTest5/No5.java
+++ PostTest5/No5.java
@@ -53,11 +53,11 @@ class Time {
     }
 
     private void timeValidation() {
-        if (this.hour < 0 && this.hour >= 24)
+        if (this.hour < 0 || this.hour >= 24)
             this.hour = 0;
-        if (this.minute < 0 && this.minute >= 60)
+        if (this.minute < 0 || this.minute >= 60)
             this.minute = 0;
-        if (this.second < 0 && this.second >= 24)
+        if (this.second < 0 || this.second >= 24)
             this.second = 0;
     }
 }
